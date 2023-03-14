ccdd7eb2f6b71ed70ffbdc84893b1777f9a1e0eb
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2023-03-14 15:34:09 +0100

    chore: package test (#13)

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,36 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+// import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const branch = core.getInput('branch')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const match = branch.match(
+      /^(?<prefix>feature|feat|fix|bugfix|hotfix|chore|patch|release|refactor)\-(?<ticket>(xxx|test)-[0-9]*)?-?(?<title>.*)$/
+    )
+
+    console.log(branch)
+
+    if (!match?.groups) {
+      console.log('Invalid branch name, skipping pre-fill')
+
+      return
+    }
+
+    const {prefix, ticket, title} = match.groups
+
+    const descriptionBody = title.replace(/-/g, ' ')
+    const formattedTicket = ticket ? ticket.toUpperCase() : undefined
+    const pullRequestTitle = `${prefix}${
+      formattedTicket ? `(${formattedTicket})` : ''
+    }: ${descriptionBody}`
+
+    core.info(`Branch name: ${branch}`)
+    core.info(`Pull request title: ${pullRequestTitle}`)
+
+    core.debug(new Date().toTimeString()) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // await wait(parseInt(ms));
+    core.info(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
