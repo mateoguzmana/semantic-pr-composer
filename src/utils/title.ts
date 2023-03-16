05ecb5942f264c81cfc8933b4ef8156e001d9b38
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2023-03-16 22:35:23 +0100

    feat: title formatting given a certain format

---
+++ src/utils/title.ts
@@ -0,0 +1,13 @@
+interface Options {
+  format: string
+  [key: string]: string
+}
+
+export function formatTitle(options: Options): string {
+  const {format, ...params} = options
+  let output = format
+  for (const key in params) {
+    output = output.replace(key, params[key])
+  }
+  return output
+}
