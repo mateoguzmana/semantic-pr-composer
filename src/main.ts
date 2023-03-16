feb3638adbe0c16d662160d866de5f63e5b36c91
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2023-03-16 19:27:31 +0100

    feat(prefill): prefill test (#15)

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,44 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 // import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
+    const {GITHUB_HEAD_REF_SLUG_URL} = process.env || {}
+
+    // eslint-disable-next-line no-console
+    console.log(GITHUB_HEAD_REF_SLUG_URL)
+
+    // eslint-disable-next-line no-console
+    console.log('testing')
+
     const branch = core.getInput('branch')
+
+    const context = github.context
+
+    const token = core.getInput('github-token')
+    const octokit = github.getOctokit(token)
+
+    if (github.context.payload.pull_request?.number) {
+      await octokit.rest.pulls.update({
+        ...context.repo,
+        pull_number: github.context.payload.pull_request.number,
+        title: 'feat(prefill): prefill test',
+        body: 'prefill test'
+      })
+    }
 
     const match = branch.match(
       /^(?<prefix>feature|feat|fix|bugfix|hotfix|chore|patch|release|refactor)\-(?<ticket>(xxx|test)-[0-9]*)?-?(?<title>.*)$/
     )
 
-    // eslint-disable-next-line no-console
-    console.log(branch)
-
     if (!match?.groups) {
       // eslint-disable-next-line no-console
-      console.log('Invalid branch name, skipping pre-fill')
+      console.log(
+        'Invalid branch name, skipping pre-fill',
+        GITHUB_HEAD_REF_SLUG_URL,
+        'nothign'
+      )
 
       return
     }
@@ -30,11 +54,15 @@ async function run(): Promise<void> {
     core.info(`Branch name: ${branch}`)
     core.info(`Pull request title: ${pullRequestTitle}`)
 
-    core.debug(new Date().toTimeString()) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    // await wait(parseInt(ms));
-    core.info(new Date().toTimeString())
+    core.setOutput('title', pullRequestTitle)
+    core.setOutput('description', descriptionBody)
 
-    core.setOutput('time', new Date().toTimeString())
+    await github.context.payload.pulls.update({
+      ...github.context.repo,
+      pull_number: github.context.payload.pull_request?.number,
+      title: pullRequestTitle,
+      body: descriptionBody
+    })
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
