2347c5735eaa635fd3151db8292829e191149e52
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2023-03-17 08:37:36 +0100

    feat: allow title format as an input (#24)

--- src/utils/title.ts
+++ src/utils/title.ts
@@ -6,8 +6,10 @@ interface Options {
 export function formatTitle(options: Options): string {
   const {format, ...params} = options
   let output = format
+
   for (const key in params) {
     output = output.replace(key, params[key])
   }
+
   return output
 }
